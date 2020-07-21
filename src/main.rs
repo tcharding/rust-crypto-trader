fdfdfcb9fcd35cffd76b1cccf34fb53b4d475f11
Tobin C. Harding <tobin@coblox.tech>
2020-07-21 18:51:27 +1000

    Add main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{thread, time::Duration};
+
+fn main() -> Result<()> {
+    println!("Rust trading bot - enjoy!");
+
+    loop {
+        println!("looping ...");
+        thread::sleep(Duration::from_secs(1));
+    }
 }
